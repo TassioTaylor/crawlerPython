df49ce959d3b8be3d950fa918487aa9cf7e14df4
tassiotaylor <tassiotaylor@gmail>
2021-12-12 01:02:34 -0300

    Coletando dados do IMDB

--- tjsp/tjsp/spiders/sp1.py
+++ tjsp/tjsp/spiders/sp1.py
@@ -1,14 +1,15 @@
 import scrapy
 
+
 class Sp1Spider(scrapy.Spider):
     name = 'sp1'
     start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
 
     def parse(self, response):
-        titulos = response.css ('.titleColumn ::text').getall()
-        for titulo in titulos:
-            yield {
-                'title': response.css ('.titleColumn a ::text').get(),
-                'ano': response.css('.secondaryInfo::text').get(),                 
+        for filmes in response.css('.titleColumn'):
+            yield{
+                'titulo': filmes.css ('.titleColumn a ::text').get(),
+                'ano': filmes.css('.secondaryInfo::text').get(),
+                'nota': response.css('strong::text').get()  
             }
